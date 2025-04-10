ccfb3282f07078876b7875a38e5c9d4babe1238c
Offphdd <pedrodalke14@gmail.com>
2025-04-09 21:56:33 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void media(float nota1, float nota2, float nota3);
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+
+float n1, n2, n3;
+
+printf("Digite a primeira nota: \n");
+scanf("%f", &n1);
+printf("Digite a segunda nota: \n");
+scanf("%f", &n2);
+printf("Digite a terceira nota: \n");
+scanf("%f", &n3);
+
+//CHAMADA DA FUNÇÃO SEM RETORNO
+media(n1, n2, n3);
+
+}
+
+void media(float nota1, float nota2, float nota3){
+    float media;
+    media = (nota1 + nota2 + nota3) / 3;
+
+    //SAÍDA DE DADOS
+printf("A média é: %.1f", media);
+
+}
